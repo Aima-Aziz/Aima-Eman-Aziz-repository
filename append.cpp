65c333068e7ea42912693d6ca6c9d2bd3f5218e3
Aima-Aziz <aimaeman874@gmail.com>
2024-12-04 10:08:48 +0500

    Add files via upload

---
+++ append.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+string str1="hello,";
+string str2="world!";
+
+//str1.append(str2);
+string result=str1.append(str2);
+cout<<"append :"<<result<<endl;
+
+
+return 0;
+
+}
