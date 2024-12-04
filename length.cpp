92a349a307070e5ff87c372b8e66a86dc0d1c9ca
Aima-Aziz <aimaeman874@gmail.com>
2024-12-04 10:09:51 +0500

    Add files via upload

---
+++ length.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{// create a string object
+	string str="hello,world!";
+//find lenth using lenghth function	
+int lenth=str.length();
+cout<<"the length of string is: "<<lenth<<endl;
+	
+return 0;	
+}
