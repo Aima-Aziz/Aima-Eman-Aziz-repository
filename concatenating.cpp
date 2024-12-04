df51b8c6b7601feeb5f0cab013f195ec39d4aae8
Aima-Aziz <aimaeman874@gmail.com>
2024-12-04 10:09:05 +0500

    Add files via upload

---
+++ concatenating.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+
+int main()
+{
+string str1="hello,";
+string str2="world!";	
+//concatenating using + operator
+
+string result=str1+str2;
+
+cout<<"concatenation string: "<<result<<endl;
+
+	
+	
+return 0;	
+}
+
+
+
